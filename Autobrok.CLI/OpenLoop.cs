20c956bf1d8f772685be4453fa15f750bbe52a75
Bogdan Romanov <bogdanchik.romanov@gmail.com>
2023-11-27 19:48:56 +0300

    fst1

---
+++ Autobrok.CLI/OpenLoop.cs
@@ -0,0 +1,46 @@
+﻿using System.IO.Enumeration;
+using System.Text.Json;
+
+public class OpenLoop
+{
+    public string Note { get; set; }
+    public DateTimeOffset CreateDate { get; set;}
+}
+
+public class OpenLoopsRepository
+{
+    private const string DirecoryName = "./openLoops/";
+    public bool Add(OpenLoop newOpenLoop)
+    {
+        Directory.CreateDirectory(DirecoryName);
+
+        var json = JsonSerializer.Serialize(newOpenLoop, new JsonSerializerOptions { WriteIndented = true });
+
+        var filename = $"{Guid.NewGuid()}.json";
+        var filePath = Path.Combine(DirecoryName, filename);
+
+        File.WriteAllText(filePath, json);
+
+        return true;
+    }
+    public OpenLoop[] Get()
+    {
+        var files = Directory.GetFiles(DirecoryName);
+        var openLoops = new List<OpenLoop>();
+
+        foreach (var file in files)
+        {
+            var json = File.ReadAllText(file);
+            var openLoop = JsonSerializer.Deserialize<OpenLoop>(json);
+            
+            if (openLoop == null)
+            {
+                throw new Exception("OpenLoop can't be serialized");
+            }
+
+            openLoops.Add(openLoop);
+        }
+        return openLoops.ToArray();
+        
+    }
+}
---
+++ Autobrok.CLI/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net.WebSockets;
+using System.Text;
+using System.Text.Json;
+
+
+Console.InputEncoding = System.Text.Encoding.Unicode;
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+var openLoopsRepository = new OpenLoopsRepository();
+
+{
+    Console.WriteLine("Что вас беспокоит сейчас?");
+
+    string? note;
+    do
+    {
+        note = Console.ReadLine();
+    } while (string.IsNullOrWhiteSpace(note));
+
+    openLoopsRepository.Add(new OpenLoop
+    {
+        Note = note,
+        CreateDate = DateTimeOffset.UtcNow
+    });
+}
+
+{
+
+    var openLoops = openLoopsRepository.Get();
+    var group = openLoops.GroupBy(x => x.CreateDate);
+    foreach (var groupOfOpenLoops in group)
+    {
+        Console.WriteLine($"Ваши заботы: {groupOfOpenLoops.Key:dd.MM.yyyy}");
+
+        foreach (var openLoop in groupOfOpenLoops.ToArray())
+        {
+            Console.WriteLine(openLoop.Note);
+        }
+        
+    }
+}
